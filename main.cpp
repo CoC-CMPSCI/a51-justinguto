e95408dc68b96a8dcbea3f853c839da9af2692bf
Kyu Lee <kwooler@gmail.com>
2026-03-11 00:22:01 -0700

    Student starter code

--- main.cpp
+++ main.cpp
@@ -7,26 +7,11 @@ using namespace std;
 int main()
 {
     // TODO: declare variable n for the exponent input
-    int n;
 
     // TODO: read n from standard input
-    cin >> n;
 
     // TODO: compute 2 to the power of n using a loop (no cmath pow)
     // For positive n: multiply result by 2, n times
     // For negative n: divide result by 2, |n| times
-    double result = 1.0;
-    if (n >= 0) {
-        for (int i = 0; i < n; i++) {
-            result *= 2.0;
-        }
-    } else {
-        for (int i = 0; i < -n; i++) {
-            result /= 2.0;
-        }
-    }
-
-    cout << setprecision(5) << fixed;
-    cout << "2 to Power " << n << " is " << result << endl;
 
 }
