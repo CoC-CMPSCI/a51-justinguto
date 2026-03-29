ffd65221938bba1050665792b2f657afdb63232e
justinguto <jutogustowski@gmail.com>
2026-03-29 11:55:14 -0700

    Complete a51 power of two program

--- main.cpp
+++ main.cpp
@@ -1,17 +1,30 @@
-// Complete the following program.
 #include <iostream>
 #include <iomanip>
-
 using namespace std;
 
 int main()
 {
-    // TODO: declare variable n for the exponent input
+    int n;
+    double result = 1.0;
 
-    // TODO: read n from standard input
+    cin >> n;
 
-    // TODO: compute 2 to the power of n using a loop (no cmath pow)
-    // For positive n: multiply result by 2, n times
-    // For negative n: divide result by 2, |n| times
+    if (n >= 0)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            result *= 2.0;
+        }
+    }
+    else
+    {
+        for (int i = 0; i < -n; i++)
+        {
+            result /= 2.0;
+        }
+    }
 
+    cout << fixed << setprecision(5) << result << endl;
+
+    return 0;
 }
